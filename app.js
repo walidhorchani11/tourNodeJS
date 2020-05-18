3a08b0e31139b6a420e6f95273743003bce160fc
Horchani Walid <walidhorchani11@gmail.com>
2020-05-18 20:07:52 +0100

    route not found

--- app.js
+++ app.js
@@ -14,5 +14,11 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 404,
+    message: `cant find route ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
