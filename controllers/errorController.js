5267c9b9d8f563db3af6cab1dcfb34c25cbdf648
Horchani Walid <walidhorchani11@gmail.com>
2020-05-19 21:05:26 +0100

    dev & prod error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,38 @@
+const sendErrorDev = (err, res) => {
+  // max of information
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // opertional , trusted error : send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // programming or other unkhown error
+    console.log('Error:', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'something went very wrong',
+    });
+  }
+
+};
+
 module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
   err.statusCode = err.statusCode || 500;
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
