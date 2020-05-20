0aef328fbc23542c74ee7256de8b86f8584b7b56
Horchani Walid <walidhorchani11@gmail.com>
2020-05-20 01:04:04 +0100

    user Model

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,28 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: [true, 'username required !'] },
-  password: { type: String, required: true, select: false },
+  name: {
+    type: String,
+    required: [true, 'please tell us your name!'],
+    trim: true,
+  },
   email: {
     type: String,
     required: true,
-    unique: [true, 'mail already exist !'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'please provide a valide email!'],
+  },
+  password: {
+    type: String,
+    required: [true, 'please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'please confirm your password'],
   },
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
