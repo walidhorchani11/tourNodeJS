061a4cfe0fda4d0cbde2f4b18b80c7e8f465022a
Horchani Walid <walidhorchani11@gmail.com>
2020-06-07 13:38:45 +0100

    debut avec multer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const multer = require('multer');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
+const upload = multer({ dest: 'public/img/walid' });
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
@@ -12,7 +14,7 @@ router.route('/').get(userController.getUsers).post(userController.createUser);
 router
   .route('/:id')
   .get(userController.getUser)
-  .patch(userController.updateUser)
+  .patch(upload.single('photo'), userController.updateUser)
   .delete(userController.deleteUser);
 
 module.exports = router;
